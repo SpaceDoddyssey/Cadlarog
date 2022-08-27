9b0749aa2cb4490b87d717a9528b5af5bbb943dc
SpaceDoddyssey <cameronradodd@gmail.com>
2022-08-27 11:27:33 -0700

    Stripped level generation code out of levelmap and put it in its own module

---
+++ source/LevelGeneration.d
@@ -0,0 +1,249 @@
+module levelgen;
+
+import ecsd;
+import components;
+import components.complex; 
+
+import levelmap;
+import entitycreation;
+import std.stdio;
+import dplug.math.vector;
+import randommodule;
+import std.random;
+import std.algorithm.searching;
+
+static void initialize(LevelMap lev){
+    writeln("Populating ", lev.maxWidth, " * ", lev.maxHeight, " tiles");
+    for(int x = 0; x < lev.maxWidth; x++){
+        for(int y = 0; y < lev.maxHeight; y++){
+            Tile T;
+            T.setPos(cast(int)(x*lev.tileSize), cast(int)(y*lev.tileSize));
+            T.mpos.x = x; T.mpos.y = y;
+            T.type = TileType.Wall;
+            lev.getTile(x,y) = T;
+        }
+    }
+}
+
+Rect[] partitionPhase(LevelMap lev, int numPartitions){
+    writeln("Partitioning ", numPartitions, " times");
+    Rect[] partitions;
+    //The first "partition" is the entire map
+    vec2i alpha = vec2i(0, 0);
+    vec2i omega = vec2i(lev.maxWidth-1, lev.maxHeight-1);
+    Rect wholeMap = Rect(alpha, omega);
+    partitions ~= (wholeMap);
+//writeln("Partitioning ", numPartitions, " times");
+    for(int i = 0; i < numPartitions; i++){
+        Rect[] newPartitions;
+        if(i % 2 == 0){ //Vertical
+            foreach(ref Rect space; partitions){
+                newPartitions ~= (space.partitionVertical());
+                for(int y = space.mins.y; y <= space.maxs.y; y++){
+                    lev.getTile(space.maxs.x+1,y).type = TileType.Floor;
+                }
+            }
+        } else { //Horizontal
+            foreach(ref Rect space; partitions){
+                newPartitions ~= (space.partitionHorizontal());
+                for(int x = space.mins.x; x <= space.maxs.x; x++){
+                    lev.getTile(x,space.maxs.y+1).type = TileType.Floor;
+                }
+            }
+        }
+        partitions ~= (newPartitions);
+    } 
+    return partitions;
+}
+
+
+//Takes the partitions and generates rooms in numRooms spaces. 
+//If numRooms is greater than the number of partitions, puts a room in every partition
+void roomGenPhase(LevelMap lev, Rect[] partitions, int numRooms){
+    int[] alreadyUsed;
+
+    //writeln("roomGenPhase");
+    for(int p=0; p<partitions.length; p++) {
+        Rect r = partitions[p];
+        //writeln("   p=", p,
+        //    ", min=(", r.mins.x, ",", r.mins.y,
+        //    "), max=(", r.maxs.x, ",", r.maxs.y, ")" );
+    }
+
+    for(int i = 0; i < numRooms && i < partitions.length; i++){
+        int nextIndex = cast(int)uniform(0, partitions.length, rand);
+        if(canFind(alreadyUsed, nextIndex)){
+            i--;
+            continue;
+        }
+        alreadyUsed ~= nextIndex;
+        Rect space = partitions[nextIndex];
+        int minX, minY;
+        //writeln("-----\n mins.x:",space.mins.x,"  mins.y:",space.mins.y,        "\n max.x:",space.maxs.x," max.y:",space.maxs.y,        "\n lev.minRoomWidth:",lev.minRoomWidth," lev.minRoomHeight:",lev.minRoomHeight,);
+        if(space.mins.x >= space.maxs.x - lev.minRoomWidth){
+            minX = cast(int)space.mins.x;
+        } else {
+            minX = cast(int)uniform(space.mins.x, space.maxs.x - lev.minRoomWidth, rand);
+        }           
+        if(space.mins.y >= space.maxs.y - lev.minRoomHeight){
+            minY = cast(int)space.mins.y;
+        } else {
+            minY = cast(int)uniform(space.mins.y, space.maxs.y - lev.minRoomHeight, rand);
+        }
+        vec2i botLeft = vec2i(minX, minY);
+
+        int topX, topY;
+        if(minX + lev.minRoomWidth >= space.maxs.x){
+            topX = space.maxs.x;
+        } else {
+            topX = cast(int)uniform(minX + lev.minRoomWidth, space.maxs.x, rand);
+        }
+        if(minY + lev.minRoomHeight >= space.maxs.y){
+            topY = space.maxs.y;
+        } else {
+            topY = cast(int)uniform(minY + lev.minRoomHeight, space.maxs.y, rand);
+        }
+        vec2i topRight = vec2i(topX, topY);
+
+        Rect newRect = Rect(botLeft, topRight);
+        lev.rooms ~= Room(newRect);
+    }
+write("Placing rooms in partitions ");
+foreach(int i ; alreadyUsed){ write(i, " "); }
+writeln("");
+    foreach(Room r; lev.rooms){
+        for(int x = r.rect.mins.x; x <= r.rect.maxs.x; x++){
+            for(int y = r.rect.mins.y; y <= r.rect.maxs.y; y++){
+                //Debug.Log("(" + x + "," + y + ")");
+                if(x == r.rect.mins.x || y == r.rect.mins.y || x == r.rect.maxs.x || y == r.rect.maxs.y){
+                    lev.getTile(x, y).type = TileType.RoomBorder;
+                } else {
+                    lev.getTile(x, y).type = TileType.Floor;
+                }
+            }
+        }
+    }
+}
+bool plumbLineToPath(LevelMap lev, Room room){
+    int side = uniform(0, 4, rand);
+    int xPos, yPos; //Position of the door to be placed
+    int xWalkDelta, yWalkDelta; //How much to offset each of these values (-1, 0, or 1) each step depending on what dir we're walking
+    if(side <= 1){ //up-down
+        xPos = uniform(room.rect.mins.x+1, room.rect.maxs.x, rand);
+        xWalkDelta = 0;
+        if(side == 0){ //up
+            yPos = room.rect.maxs.y;
+            yWalkDelta = 1;
+        } else {
+            yPos = room.rect.mins.y;
+            yWalkDelta = -1;
+        }
+    } else { //left-right
+        yPos = uniform(room.rect.mins.y+1, room.rect.maxs.y, rand);
+        yWalkDelta = 0;
+        if(side == 3){ //left
+            xPos = room.rect.mins.x;
+            xWalkDelta = -1;
+        } else {
+            xPos = room.rect.maxs.x;
+            xWalkDelta = 1;
+        }
+    }
+
+    int nextX = xPos;
+    int nextY = yPos;
+    int attempts = 0;
+    while(true){
+        if(attempts++ >= 100){
+            writeln("Aborted! ", __FILE__, " line ", __LINE__);
+            return false;
+        }
+        nextX += xWalkDelta;
+        nextY += yWalkDelta;
+        if(!(nextX < lev.maxWidth && nextX > 0 && nextY < lev.maxHeight && nextY > 0)){
+            return false;
+        } else if (lev.getTile(nextX, nextY).type == TileType.Floor) {
+            break;
+        }
+    }
+
+    //If we're here, we've hit a path. Walk back and change tiles to floor.
+    while(nextX != xPos || nextY != yPos){
+        nextX -= xWalkDelta;
+        nextY -= yWalkDelta;
+        lev.getTile(nextX, nextY).type = TileType.Floor;
+    }
+
+    //now place a door there
+    Entity door = makeEntity(lev.verse, "door", null, xPos, yPos);
+    lev.getTile(xPos, yPos).add(door);
+
+    return true;
+}
+
+static void cullDeadEnds(LevelMap lev){
+    writeln("Culling dead ends");
+    for(int x = 1; x < lev.maxWidth; x++){ //First check top and bottom
+        if(lev.getTile(x, 0).type == TileType.Floor){
+            int y = 0;
+            while(lev.getTile(x+1, y).type != TileType.Floor && lev.getTile(x-1, y).type != TileType.Floor){
+                lev.getTile(x, y).type = TileType.Wall;
+                y++;
+            }
+        } 
+        if (lev.getTile(x, lev.maxHeight-1).type == TileType.Floor){
+            int y = lev.maxHeight - 1;
+            while(lev.getTile(x+1, y).type != TileType.Floor && lev.getTile(x-1, y).type != TileType.Floor){
+                lev.getTile(x, y).type = TileType.Wall;
+                y--;
+            }
+        }
+    }
+    for(int y = 1; y < lev.maxHeight; y++){ //Now check left and right
+        if(lev.getTile(0, y).type == TileType.Floor){
+            int x = 0;
+            while(lev.getTile(x, y+1).type != TileType.Floor && lev.getTile(x, y-1).type != TileType.Floor){
+                lev.getTile(x, y).type = TileType.Wall;
+                x++;
+            }
+        }
+        if (lev.getTile(lev.maxWidth-1, y).type == TileType.Floor){
+            int x = lev.maxWidth - 1;
+            while(lev.getTile(x, y+1).type != TileType.Floor && lev.getTile(x, y-1).type != TileType.Floor){
+                lev.getTile(x, y).type = TileType.Wall;
+                x--;
+            }
+        }
+    }
+}
+
+static void populate(LevelMap lev){
+    writeln("Populating entities");
+    Room r = lev.placeEntInRandomRoom("Player", null);
+    lev.placeEntInRandomRoom("crate", "sword");
+    lev.placeEntInRoom("crate", "sword", r);
+    lev.placeEntInRoom("crate", "shield", r);
+    lev.placeEntInRoom("slime_purple", null, r);
+    lev.placeEntInRoom("slime_green", null, r);
+    lev.placeEntInRandomRoom("stairs_down", null);
+    if(lev.levelNum != 0){
+        lev.placeEntInRandomRoom("stairs_up", null);
+    }
+}
+
+void texturePhase(LevelMap lev){
+    import rendermodule: SpriteRender, SpriteLayer;
+    foreach(t; lev.tiles){
+        string target;
+        switch(t.type){
+            case(TileType.Floor):
+                target = "sprites/Floor.png"; break;
+            case(TileType.RoomBorder):
+                target = "sprites/RoomBorder.png"; break;
+            default:
+                target = "sprites/Empty.png"; break;
+        }
+        t.tileEnt = makeEntity(lev.verse, "Tile", null, t.pos.position.x, t.pos.position.y);
+        t.tileEnt.add(SpriteRender(target, vec2i(32, 32), SpriteLayer.Floor));
+    }
+}
--- source/LevelMap.d
+++ source/LevelMap.d
@@ -14,6 +14,7 @@ import playermodule;
 import set;
 import game;
 import randommodule;
+import levelgen;
 
 import vibe.data.serialization;
 import dplug.math.vector;
@@ -68,32 +69,19 @@ class LevelMap{
             maxWidth = _x;
             maxHeight = _y;
             tiles = new Tile[_x * _y];
-            initialize();
-            Rect[] partitions = partitionPhase(4);
-            roomGenPhase(partitions, 11);
+            initialize(this);
+            Rect[] partitions = partitionPhase(this, 4);
+            roomGenPhase(this, partitions, 11);
             writeln("Connecting paths to rooms");
             foreach(Room r ; rooms){
-               while(!plumbLineToPath(r)){}
-               while(!plumbLineToPath(r)){}
+               while(!plumbLineToPath(this, r)){}
+               while(!plumbLineToPath(this, r)){}
             }
-            cullDeadEnds();
-            texturePhase();
+            cullDeadEnds(this);
+            texturePhase(this);
         }
     ref Tile getTile(int x, int y) { return tiles[y * maxWidth + x]; }
     ref Tile getTile(vec2i pos) { return getTile(pos.v.tupleof); }
-    void populate(){
-        writeln("Populating entities");
-        Room r = placeEntInRandomRoom("Player", null);
-        placeEntInRandomRoom("crate", "sword");
-        placeEntInRoom("crate", "sword", r);
-        placeEntInRoom("crate", "shield", r);
-        placeEntInRoom("slime_purple", null, r);
-        placeEntInRoom("slime_green", null, r);
-        placeEntInRandomRoom("stairs_down", null);
-        if(levelNum != 0){
-            placeEntInRandomRoom("stairs_up", null);
-        }
-    }
     void lookForEnts(){ //debug function
         foreach(Tile t ; tiles){
             if(t.ents.length() != 0){
@@ -114,23 +102,22 @@ class LevelMap{
         getTile(source).remove(e);
         getTile(dest).add(e);
     }
-    private Room placeEntInRandomRoom(string s, string s2){
+    Room placeEntInRandomRoom(string s, string s2){
         int whichRoom = cast(int)uniform(0, rooms.length, rand);
         Room r = rooms[whichRoom];
         int attempts = 0;
         while(attempts < 1000){
-            int entX = cast(int)uniform(r.rect.mins.x+1, r.rect.maxs.x, rand);
-            int entY = cast(int)uniform(r.rect.mins.y+1, r.rect.maxs.y, rand);
-            Tile t = getTile(entX, entY);
+            vec2i randVec = r.randPointIn();
+            Tile t = getTile(randVec);
             if((t.entsWith!TileBlock).length > 0){
                 continue;
             } else {
-                Entity pEnt = makeEntity(verse, s, s2, entX, entY);
+                Entity pEnt = makeEntity(verse, s, s2, randVec.x, randVec.y);
                 //writeln("Spawning entity at ", entX, " ", entY );
-                getTile(entX, entY).add(pEnt);
+                getTile(randVec).add(pEnt);
                 if(s == "Player"){
-                    cameraXOffset = entX - 15;
-                    cameraYOffset = entY - 15;
+                    cameraXOffset = randVec.x - 15;
+                    cameraYOffset = randVec.x - 15;
                 }
                 break;
             }
@@ -138,237 +125,21 @@ class LevelMap{
         return r;
         //Expand -------------------------------
     }
-    private void placeEntInRoom(string s, string s2, Room r){
+    void placeEntInRoom(string s, string s2, Room r){
         int attempts = 0;
         while(attempts < 1000){
             attempts++;
-            int entX = cast(int)uniform(r.rect.mins.x+1, r.rect.maxs.x, rand);
-            int entY = cast(int)uniform(r.rect.mins.y+1, r.rect.maxs.y, rand);
-            Tile t = getTile(entX, entY);
+            vec2i randVec = r.randPointIn();
+            Tile t = getTile(randVec);
             if((t.entsWith!TileBlock).length > 0){
                 continue;
             } else {
-                Entity pEnt = makeEntity(verse, s, s2, entX, entY);
-                getTile(entX, entY).add(pEnt);
+                Entity pEnt = makeEntity(verse, s, s2, randVec.x, randVec.y);
+                getTile(randVec).add(pEnt);
                 return;
             }
         }
         writeln("Entity failed to place!");
-    }
-    private void initialize(){
-        writeln("Populating ", maxWidth, " * ", maxHeight, " tiles");
-        for(int x = 0; x < maxWidth; x++){
-            for(int y = 0; y < maxHeight; y++){
-                Tile T;
-                T.setPos(cast(int)(x*tileSize), cast(int)(y*tileSize));
-                T.mpos.x = x; T.mpos.y = y;
-                T.type = TileType.Wall;
-                getTile(x,y) = T;
-            }
-        }
-    }
-    private Rect[] partitionPhase(int numPartitions){
-        writeln("Partitioning ", numPartitions, " times");
-        Rect[] partitions;
-        //The first "partition" is the entire map
-        vec2i alpha = vec2i(0, 0);
-        vec2i omega = vec2i(maxWidth-1, maxHeight-1);
-        Rect wholeMap = Rect(alpha, omega);
-        partitions ~= (wholeMap);
-//writeln("Partitioning ", numPartitions, " times");
-        for(int i = 0; i < numPartitions; i++){
-            Rect[] newPartitions;
-            if(i % 2 == 0){ //Vertical
-                foreach(ref Rect space; partitions){
-                    newPartitions ~= (space.partitionVertical());
-                    for(int y = space.mins.y; y <= space.maxs.y; y++){
-                        getTile(space.maxs.x+1,y).type = TileType.Floor;
-                    }
-                }
-            } else { //Horizontal
-                foreach(ref Rect space; partitions){
-                    newPartitions ~= (space.partitionHorizontal());
-                    for(int x = space.mins.x; x <= space.maxs.x; x++){
-                        getTile(x,space.maxs.y+1).type = TileType.Floor;
-                    }
-                }
-            }
-            partitions ~= (newPartitions);
-        } 
-        return partitions;
-    }
-    //Takes the partitions and generates rooms in numRooms spaces. 
-    //If numRooms is greater than the number of partitions, puts a room in every partition
-    private void roomGenPhase(Rect[] partitions, int numRooms){
-        int[] alreadyUsed;
-
-        //writeln("roomGenPhase");
-        for(int p=0; p<partitions.length; p++) {
-            Rect r = partitions[p];
-            //writeln("   p=", p,
-            //    ", min=(", r.mins.x, ",", r.mins.y,
-            //    "), max=(", r.maxs.x, ",", r.maxs.y, ")" );
-        }
-
-        for(int i = 0; i < numRooms && i < partitions.length; i++){
-            int nextIndex = cast(int)uniform(0, partitions.length, rand);
-            if(canFind(alreadyUsed, nextIndex)){
-                i--;
-                continue;
-            }
-            alreadyUsed ~= nextIndex;
-            Rect space = partitions[nextIndex];
-            int minX, minY;
-            //writeln("-----\n mins.x:",space.mins.x,"  mins.y:",space.mins.y,        "\n max.x:",space.maxs.x," max.y:",space.maxs.y,        "\n minRoomWidth:",minRoomWidth," minRoomHeight:",minRoomHeight,);
-            if(space.mins.x >= space.maxs.x - minRoomWidth){
-                minX = cast(int)space.mins.x;
-            } else {
-                minX = cast(int)uniform(space.mins.x, space.maxs.x - minRoomWidth, rand);
-            }           
-            if(space.mins.y >= space.maxs.y - minRoomHeight){
-                minY = cast(int)space.mins.y;
-            } else {
-                minY = cast(int)uniform(space.mins.y, space.maxs.y - minRoomHeight, rand);
-            }
-            vec2i botLeft = vec2i(minX, minY);
-
-            int topX, topY;
-            if(minX + minRoomWidth >= space.maxs.x){
-                topX = space.maxs.x;
-            } else {
-                topX = cast(int)uniform(minX + minRoomWidth, space.maxs.x, rand);
-            }
-            if(minY + minRoomHeight >= space.maxs.y){
-                topY = space.maxs.y;
-            } else {
-                topY = cast(int)uniform(minY + minRoomHeight, space.maxs.y, rand);
-            }
-            vec2i topRight = vec2i(topX, topY);
-
-            Rect newRect = Rect(botLeft, topRight);
-            rooms ~= Room(newRect);
-        }
-write("Placing rooms in partitions ");
-foreach(int i ; alreadyUsed){ write(i, " "); }
-writeln("");
-        foreach(Room r; rooms){
-            for(int x = r.rect.mins.x; x <= r.rect.maxs.x; x++){
-                for(int y = r.rect.mins.y; y <= r.rect.maxs.y; y++){
-                    //Debug.Log("(" + x + "," + y + ")");
-                    if(x == r.rect.mins.x || y == r.rect.mins.y || x == r.rect.maxs.x || y == r.rect.maxs.y){
-                        getTile(x, y).type = TileType.RoomBorder;
-                    } else {
-                        getTile(x, y).type = TileType.Floor;
-                    }
-                }
-            }
-        }
-    }
-    private bool plumbLineToPath(Room room){
-        int side = uniform(0, 4, rand);
-        int xPos, yPos; //Position of the door to be placed
-        int xWalkDelta, yWalkDelta; //How much to offset each of these values (-1, 0, or 1) each step depending on what dir we're walking
-        if(side <= 1){ //up-down
-            xPos = uniform(room.rect.mins.x+1, room.rect.maxs.x, rand);
-            xWalkDelta = 0;
-            if(side == 0){ //up
-                yPos = room.rect.maxs.y;
-                yWalkDelta = 1;
-            } else {
-                yPos = room.rect.mins.y;
-                yWalkDelta = -1;
-            }
-        } else { //left-right
-            yPos = uniform(room.rect.mins.y+1, room.rect.maxs.y, rand);
-            yWalkDelta = 0;
-            if(side == 3){ //left
-                xPos = room.rect.mins.x;
-                xWalkDelta = -1;
-            } else {
-                xPos = room.rect.maxs.x;
-                xWalkDelta = 1;
-            }
-        }
-
-        int nextX = xPos;
-        int nextY = yPos;
-        int attempts = 0;
-        while(true){
-            if(attempts++ >= 100){
-                writeln("Aborted! ", __FILE__, " line ", __LINE__);
-                return false;
-            }
-            nextX += xWalkDelta;
-            nextY += yWalkDelta;
-            if(!(nextX < maxWidth && nextX > 0 && nextY < maxHeight && nextY > 0)){
-                return false;
-            } else if (getTile(nextX, nextY).type == TileType.Floor) {
-                break;
-            }
-        }
-
-        //If we're here, we've hit a path. Walk back and change tiles to floor.
-        while(nextX != xPos || nextY != yPos){
-            nextX -= xWalkDelta;
-            nextY -= yWalkDelta;
-            getTile(nextX, nextY).type = TileType.Floor;
-        }
-
-        //now place a door there
-        Entity door = makeEntity(verse, "door", null, xPos, yPos);
-        getTile(xPos, yPos).add(door);
-
-        return true;
-    }
-    private void cullDeadEnds(){
-        writeln("Culling dead ends");
-        for(int x = 1; x < maxWidth; x++){ //First check top and bottom
-            if(getTile(x, 0).type == TileType.Floor){
-                int y = 0;
-                while(getTile(x+1, y).type != TileType.Floor && getTile(x-1, y).type != TileType.Floor){
-                    getTile(x, y).type = TileType.Wall;
-                    y++;
-                }
-            } 
-            if (getTile(x, maxHeight-1).type == TileType.Floor){
-                int y = maxHeight - 1;
-                while(getTile(x+1, y).type != TileType.Floor && getTile(x-1, y).type != TileType.Floor){
-                    getTile(x, y).type = TileType.Wall;
-                    y--;
-                }
-            }
-        }
-        for(int y = 1; y < maxHeight; y++){ //Now check left and right
-            if(getTile(0, y).type == TileType.Floor){
-                int x = 0;
-                while(getTile(x, y+1).type != TileType.Floor && getTile(x, y-1).type != TileType.Floor){
-                    getTile(x, y).type = TileType.Wall;
-                    x++;
-                }
-            }
-            if (getTile(maxWidth-1, y).type == TileType.Floor){
-                int x = maxWidth - 1;
-                while(getTile(x, y+1).type != TileType.Floor && getTile(x, y-1).type != TileType.Floor){
-                    getTile(x, y).type = TileType.Wall;
-                    x--;
-                }
-            }
-        }
-    }
-    private void texturePhase(){
-        foreach(t; tiles){
-            string target;
-            switch(t.type){
-                case(TileType.Floor):
-                    target = "sprites/Floor.png"; break;
-                case(TileType.RoomBorder):
-                    target = "sprites/RoomBorder.png"; break;
-                default:
-                    target = "sprites/Empty.png"; break;
-            }
-            t.tileEnt = makeEntity(verse, "Tile", null, t.pos.position.x, t.pos.position.y);
-            t.tileEnt.add(SpriteRender(target, vec2i(32, 32), SpriteLayer.Floor));
-        }
     }
 }
 
@@ -459,6 +230,11 @@ public struct Rect
 
         maxs = firstTop;
         return top;
+    }
+    public vec2i randPointIn(){
+        int resultX = cast(int)uniform(mins.x+1, maxs.x, rand);
+        int resultY = cast(int)uniform(mins.y+1, maxs.y, rand);
+        return vec2i(resultX, resultY);
     }
 }
 
